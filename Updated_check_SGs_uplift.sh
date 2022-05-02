a98277f684294e233a7d8caf0ef98849b44b3783
Murthy1333 <104747836+Murthy1333@users.noreply.github.com>
2022-05-02 10:13:20 +0530

    updated_check_SGs_uplift.sh

---
+++ Updated_check_SGs_uplift.sh
@@ -0,0 +1,16 @@
+##############################################################################################
+#!bin/bash                                                                                   #
+# Author : Murthy                                                                            #
+##############################################################################################
+cat /etc/hosts > /home/test/originalhostsfile.txt
+cat /home/test/originalhostsfile.txt |grep -i svc | awk '{ print $1,$2 }' | sort | uniq > /home/test/svc.txt ; cat /home/test/svc.txt | awk '{ print $2 }' > /home/test/svcip.txt
+cat /home/test/originalhostsfile.txt |grep -i db | awk '{ print $1,$2 }' | sort | uniq > /home/test/db.txt ; cat /home/test/db.txt | awk '{ print $2 }' > /home/test/dbip.txt
+cat /home/test/originalhostsfile.txt |grep -i scp | awk '{ print $1,$2 }' | sort | uniq > /home/test/scp.txt ; cat /home/test/scp.txt | awk '{ print $2 }' > /home/test/scpip.txt
+for i in `cat /home/test/svcip.txt`; do ssh -i /root/.ssh/vm_private_key cloud-user@$i -T "cat /etc/redhat-release">/home/test/re1.txt ;echo " ";echo " ";echo "SVC INSTANCE IS.....$i     ";cat /home/test/re1.txt |awk '{ print $0 }';echo " ";  done
+for i in `cat /home/test/dbip.txt`; do ssh litp-admin@$i -T "cat /etc/redhat-release" >/home/test/re2.txt ;echo " ";echo " ";echo "DB INSTANCE IS.....$i     ";cat /home/test/re2.txt |awk '{ print $0 }';echo " ";  done
+for i in `cat /home/test/scpip.txt`; do ssh -i /root/.ssh/vm_private_key cloud-user@$i -T "cat /etc/redhat-release">/home/test/re3.txt ;echo " ";echo " ";echo "SCP INSTANCE IS.....$i     ";cat /home/test/re3.txt |awk '{ print $0 }';echo " ";  done
+###############################################################################################
+##THE FILE MUST RUN ON /home/test/                                                            #
+##############################################END##############################################
+
+
